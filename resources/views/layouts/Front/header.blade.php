a169d613d2a9e49af3e52d5fc3d36aa78cf2f6d3
GranPeradisoos <granparadiso729@gmail.com>
2024-08-08 07:01:33 +0600

    Home Page

---
+++ resources/views/layouts/Front/header.blade.php
@@ -0,0 +1,10 @@
+   <!-- Additional CSS Files -->
+   <link rel="stylesheet" type="text/css" href="{{asset('storage/front/assets/css/bootstrap.min.css')}}">
+
+   <link rel="stylesheet" type="text/css" href="{{asset('storage/front/assets/css/font-awesome.css')}}">
+
+   <link rel="stylesheet" href="{{asset('storage/front/assets/css/templatemo-hexashop.css')}}">
+
+   <link rel="stylesheet" href="{{asset('storage/front/assets/css/owl-carousel.css')}}">
+
+   <link rel="stylesheet" href="{{asset('storage/front/assets/css/lightbox.css')}}">
